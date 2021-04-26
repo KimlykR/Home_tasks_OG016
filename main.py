207859c8412022a4fe11f61d8f85481a56c16c96
KimlykR <roma.kema@gmail.com>
2021-04-26 22:09:18 +0300

    Калькулятор

---
+++ main.py
@@ -0,0 +1,19 @@
+# Дебильный кулькулятор
+what = input("Что делаем? (+,-,*, :):")
+a = float(input("Введи первое число:"))
+b = float(input("Введи второе число:"))
+
+if what == "+":
+    с = a + b
+    print("Результат:" + str(с))
+elif    what == "-":
+    с = a - b
+    print("Результат:" + str(с))
+elif what == "*":
+    c = a * b
+    print("Результат:" + str(c))
+elif what == ":":
+    c = a / b
+    print("Результат:" + str(c))
+else:
+    print("Ошибка ввода")
