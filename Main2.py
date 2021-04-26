1b4439f0a52f2d0bbef07f245d8c201cb216e2dd
KimlykR <roma.kema@gmail.com>
2021-04-27 02:04:55 +0300

    А если так? GIT_EDITION

---
+++ Main2.py
@@ -0,0 +1,16 @@
+# А если так? GIT_EDITION
+a = input("Введите переменную №1:")
+b = input("Введите переменную №2:")
+
+
+def XD (a,b):
+    if a<b:
+        print("You're LOOSER")
+    elif a>b:
+        print("Successful")
+    elif a==b:
+        print("Almost!")
+
+
+
+XD(a,b)
